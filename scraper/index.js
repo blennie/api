1bed6f8f6a83fdafaf8adc027d327603c2aa443c
DRIPS MEMES BLENNIE BLESSED TUNA <reinhardtuna@mail.uk>
2022-06-14 14:02:12 +0200

    api

---
+++ scraper/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	pinterest: require('./pinterest'),
+	igDownload: require('./igdl'),
+	mediafireDl: require('./mediafire'),
+	doujindesu: require('./doujindesu'),
+	pinterestdl: require('./pinterestdl'),
+	scdl: require('./scdl'),
+	randomTiktok: require('./tiktok_search'),
+	sfiledl: require('./solidfiles'),
+	tebakgambar: require('./tebakgambar'),
+	konachan: require('./konachan'),
+}
